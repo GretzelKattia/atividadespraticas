960cb7646c7bf5def89be14e095faa570b61cd68
Gretzel Kattia <gretzelpenaloza@gmail.com>
2025-08-19 12:32:36 -0300

    Atividade pratica 1

---
+++ atividadepratica01/questao04.py
@@ -0,0 +1,6 @@
+nome_do_produto = "Cadeira Infantil"
+preco_unitario = 12.4
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+print(f"O preço total da compra de {nome_do_produto} é de: R$ {preco_total:.2f}")
